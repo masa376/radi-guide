13037ded3c60648fc0ed8aeae0af9d5d4b128645
masa376 <GitHub>
2026-06-09 15:30:32 +0900

    README修正：シーダーコマンド・文末表現追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             ExaminationSeeder::class,
             ChecklistSeeder::class,
